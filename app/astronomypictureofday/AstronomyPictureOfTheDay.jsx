3e9a43b4fea8aa642becc74eef57186c8e577933
goodbyepavlyi <goodbyepavlyi@gmail.com>
2023-09-23 10:36:20 +0200

    astronomys

--- app/astronomypictureofday/AstronomyPictureOfTheDay.jsx
+++ app/astronomypictureofday/AstronomyPictureOfTheDay.jsx
@@ -55,14 +55,4 @@ export default function AstronomyPictureOfTheDay() {
             </div>
         </>
     );
-    {
-        /* <img src={astronomyPicture.url} className={"max-w-4xl"}></img>
-    
-                    <div className={"mx-4"}>
-                        <p className={"text-md text-indigo-500"}>{astronomyPicture.date}</p>
-                        <p className={"text-3xl font-semibold mb-2"}>{astronomyPicture.title}</p>
-                        <p className={"text-1xl mb-4"}>Copyright: {astronomyPicture.copyright}</p>
-                        <p className={"text-lg"}>{astronomyPicture.explanation}</p>
-                    </div> */
-    }
 }
